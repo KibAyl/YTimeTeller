fed03053e8b84abfe33d96605acb3de1304bdad1
KibAyl <kibr.a@keplerlab.io>
2023-05-22 19:42:30 +0300

    set up Package.swift

---
+++ Sources/YTimeTeller/YTimeTeller.swift
@@ -0,0 +1,6 @@
+public struct YTimeTeller {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/YTimeTellerTests/YTimeTellerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import YTimeTeller
+
+final class YTimeTellerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(YTimeTeller().text, "Hello, World!")
+    }
+}
